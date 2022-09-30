104d7becbe945eada127a747e93cf78101023e61
Marine <5641@holbertonstudents.com>
2022-09-30 12:44:53 +0200

    6th step 1st attempt

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * printf ' with proper grammar, but the outcome is a piece of art,'
+ * return: 0
+ */
+
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of ar,\n");
+return (0);
+}
