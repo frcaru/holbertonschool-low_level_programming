3c2b0fafd3300643201a300865018754a6be5db9
Marine <5641@holbertonstudents.com>
2022-10-03 16:08:35 +0200

    3 task 4th attempt

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,13 @@
 int main(void)
 {
 	char ch = 'a';
+	char ch1 = 'A';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	putchar('\n');
 
-	char ch1 = 'A';
-
-		for (ch1 = 'A'; ch1 <= 'Z'; ch1++)
+	for (ch1 = 'A'; ch1 <= 'Z'; ch1++)
 			putchar(ch1);
 	putchar('\n');
 
