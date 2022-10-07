87d33aec0c71c26629468bd35ed45074ce469416
Marine <5641@holbertonstudents.com>
2022-10-07 20:26:35 +0200

    0 task 1st attempt

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * times_table - func to prints the last digit
+ *@c : input
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if ( c >= 'A' && c <= 'Z' )
+	{
+		return(1);
+}
+	else
+	{
+		return(0);
+	}
+}
