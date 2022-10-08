6e5df6602aacb16bed0aa2e89b9f3d138565453b
Marine <5641@holbertonstudents.com>
2022-10-08 12:02:12 +0200

    6 task 1 st attempt

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_line - func to print a straight line
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
