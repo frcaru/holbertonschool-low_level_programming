25dc3380d2140dc32f235caef4ef0609bb3565d9
Marine <5641@holbertonstudents.com>
2022-10-08 17:07:53 +0200

    8 task 1 attempt

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_square - func to print a square
+ *@size:  input
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int s;
+	int q;
+
+	for (s = 0; s < size; s++)
+	{
+		for (q = 0; q < size; q++)
+		{
+			if (size > 0)
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
