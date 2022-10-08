798b6b2e7dd70da0cc0272f833e6414773e0e17f
Marine <5641@holbertonstudents.com>
2022-10-08 16:24:06 +0200

    7 task 1 attempt

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_diagonal - func to print a diagonal line
+ *@n:  input
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
