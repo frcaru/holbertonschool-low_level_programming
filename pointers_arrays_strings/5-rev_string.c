95a89d7d8f67cf30d77b05aea7fd560dfb11d2bd
Marine <5641@holbertonstudents.com>
2022-10-11 15:55:26 +0200

    Update

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - prints any string in reverse
+ * @s: input
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for(; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
