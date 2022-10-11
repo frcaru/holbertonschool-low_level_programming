70250d8618f2eb7da8147f38715f317f8d1ebfc6
Marine <5641@holbertonstudents.com>
2022-10-11 22:05:18 +0200

    7 task correction

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,20 +12,10 @@ void puts_half(char *str)
 	int i = 0;
 	int l;
 
-
-	while (str[i])
-	{
+	while (str[i + 1])
 		i++;
-	}
 
-	if (i % 2 == 0)
-	{
-		l = (i + 1) / 2;
-	}
-	else
-	{
-		l = i / 2;
-	}
+	l = (i / 2) + 1;
 
 	for (; str[l]; l++)
 	{
