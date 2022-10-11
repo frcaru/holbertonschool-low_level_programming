f7d2e96deac3f7714f36f69722f291d53ed2efa8
Marine <5641@holbertonstudents.com>
2022-10-11 06:24:14 +0200

    2 task 1st attempt

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strlen - returns the lenght of a string
+ *@s: input
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
+	return (c);
+}
