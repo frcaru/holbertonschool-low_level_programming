40d76b95d3e4efaffc1a566a356248c642970da5
Marine <5641@holbertonstudents.com>
2022-10-16 15:02:12 +0200

    4 task 1st attempt

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: input
+ * @n: input
+ *
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int f;
+
+	for (f = 0; f < n; f++)
+	{
+		n--;
+		tmp = a[f];
+		a[f] = a[n];
+		a[n] = tmp;
+	}
+	
+}
