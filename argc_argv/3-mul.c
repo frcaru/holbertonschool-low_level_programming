9cf5eb760dccbeb888dd8cf9505344d61ace3725
Marine <5641@holbertonstudents.com>
2022-10-17 18:14:14 +0200

    3 task betty correction

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,16 +12,12 @@ int main(int argc, char **argv)
 {
 	int sum;
 
-
 	if (argc == 3)
 	{
-			sum = atoi(argv[1]) * atoi(argv[2]);
-
+		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
-
 		return (0);
 	}
-
 
 	else
 	{
