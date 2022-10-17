7966fee3c5a6e706ff45ff7679341f0797e275cc
Marine <5641@holbertonstudents.com>
2022-10-17 15:11:20 +0200

    0 task 2nd attempt

---
+++ static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * strcpy - copies
+ * @s: input
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	(void)s;
+    return (0);
+}
---
+++ static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * strcpy - copies
+ * @s: input
+ *
+ * Return: 0
+ */
+
+void _puts(char *s)
+{
+	(void)s;
+}
