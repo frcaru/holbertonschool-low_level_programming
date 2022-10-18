4c1027633895a0c128f2f91dc419f2999ea2d512
Marine <5641@holbertonstudents.com>
2022-10-18 12:33:37 +0200

    2 task 1st attempt

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: input
+ *
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+	return (1) + _strlen_recursion(s + 1);
+	}
+	else
+		return (0);
+}
