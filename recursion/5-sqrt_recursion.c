dfbe3dc6a4431f236bf1ca20fa56fccd0167a1f3
Marine <5641@holbertonstudents.com>
2022-10-19 12:09:45 +0200

    5 task betty correction

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,24 +1,33 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the square root of n
- * @n: input
+ * funcmarine - finds the square root of x
+ * @x: input
+ * @y: input
  *
  * Return: 0
  */
+
 int funcmarine(int x, int y)
 {
 	if ((y * y) == x)
 		return (y);
 	if ((y * y) < x)
-		return funcmarine(x, y + 1);
+		return (funcmarine(x, y + 1));
 	if (y > x)
 		return (-1);
 	return (-1);
 
 }
 
+/**
+ * _sqrt_recursion - returns the square root of n
+ * @n: input
+ *
+ * Return: 0
+ */
+
 int _sqrt_recursion(int n)
 {
-	return funcmarine(n, 1);
+	return (funcmarine(n, 1));
 }
