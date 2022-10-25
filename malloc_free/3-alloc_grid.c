eab383e94d95be9314276c37e6d9e4b5a49ce721
Marine <5641@holbertonstudents.com>
2022-10-25 18:58:24 +0200

    3 task 1st gpush

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - returns a pointer to a pointer
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: the pointer that will be created
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	p = malloc(sizeof(int*) * height);
+	if (p == 0)
+	{
+		free(p);
+		p = 0;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+		if (p[i] == 0)
+		{
+			for (; i >= 0; i--)
+			{
+				free(p[i]);
+				p[i] = 0;
+			}
+			free(p);
+			p = 0;
+		}
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+	return (p);
+}
