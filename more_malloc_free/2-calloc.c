2eae0d1dee94ab24628541cdecd71884c43b8ccf
Marine <5641@holbertonstudents.com>
2022-10-28 14:34:37 +0200

    2 task 1st push

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: input
+ * @size: input
+ *
+ * Return: 0
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+
+	p = malloc(nmemb * size);
+	if (p == 0)
+	{
+		free(p);
+		p = 0;
+		return (0);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		p[1] = '\0';
+	}
+	return (p);
+}
+
+
+
