57005a41e8b1d6bbccd6a0b10e3c31a8976dde52
Marine <5641@holbertonstudents.com>
2022-10-28 13:41:42 +0200

    1 task 1st push

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @n: input
+ *
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int size = 0;
+	char *s1et2 = 0;
+
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2 != NULL && s2[j] != '\0'; j++)
+		;
+
+	size = i + n;
+	s1et2 = malloc(sizeof(char) * size);
+	if (s1et2 == 0)
+		return (0);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s1et2[i] = s1[i];
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		s1et2[i] = s2[j];
+		i++;
+	}
+	s1et2[i] = '\0';
+	return (s1et2);
+}
