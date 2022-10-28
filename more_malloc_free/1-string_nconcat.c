f9cabdd437cd782338e208274f51c502e7b5a9d0
Marine <5641@holbertonstudents.com>
2022-10-28 13:49:34 +0200

    1 task 2nd push

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size = 0;
 	char *s1et2 = 0;
 
+	if (s1 == 0)
+		s1 = "";
+	if (s2 = 0)
+		s2 = "";
+
 	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2 != NULL && s2[j] != '\0'; j++)
