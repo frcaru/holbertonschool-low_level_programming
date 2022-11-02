44f530635348661391a381416b22fdcbb6698a67
Marine <5641@holbertonstudents.com>
2022-11-02 15:40:21 +0100

    4 task 18 000 th attempt de miiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiierdaaaaaaaav

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -25,23 +25,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = malloc(strlen(name) + 1);
 	if (d->name == 0)
 	{
-		free(d->name);
-		d->name = 0;
+		free(d);
+		d = 0;
 		return (0);
 	}
 	d->owner = malloc(strlen(owner) + 1);
 	if (d->owner == 0)
 	{
-		free(d->owner);
-		d->owner = 0;
+		free(d);
+		d = 0;
 		return (0);
 	}
 
 	strcpy(d->name, name);
 	strcpy(d->owner, owner);
-
-	if (name == 0 || owner == 0)
-		return (0);
 
 	d->age = age;
 
