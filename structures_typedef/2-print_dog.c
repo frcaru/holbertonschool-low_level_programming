236a153b8302acc26064a4aa2958fc86b9f1ed87
Marine <5641@holbertonstudents.com>
2022-11-02 14:06:35 +0100

    2 task 1st push

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a variable of type struct dog
+ * @d: variable of struct dog type
+ *
+ * Return: 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+		return;
+
+	if (d->name == 0)
+		printf("Name: %s\n", d->name);
+	printf("Name: %s\n", d->name);
+
+	if (d->age == 0)
+		printf("Age: %fd\n", d->age);
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == 0)
+		printf("Owner: %s\n", d->owner);
+	printf("Owner: %s\n", d->owner);
+}
