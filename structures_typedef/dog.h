13caf3b51f5970bd110ecd2044c513a2e374db34
Marine <5641@holbertonstudents.com>
2022-11-02 11:32:31 +0100

    0 task add comments

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog's name, age and owners
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+
 struct dog
 {
 	char *name;
