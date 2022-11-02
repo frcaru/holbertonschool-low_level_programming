821446cb41d0fde3bad478145416ee5da86ad60b
Marine <5641@holbertonstudents.com>
2022-11-02 14:09:50 +0100

    2 task correction if a value of d is null

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,14 +14,14 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == 0)
-		printf("Name: (nil)\n", d->name);
+		printf("Name: (nil)\n");
 	printf("Name: %s\n", d->name);
 
 	if (d->age == 0)
-		printf("Age: (nil)\n", d->age);
+		printf("Age: (nil)\n");
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == 0)
-		printf("Owner: (nil)\n", d->owner);
+		printf("Owner: (nil)\n");
 	printf("Owner: %s\n", d->owner);
 }
