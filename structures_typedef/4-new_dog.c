3bfc4a4ccd82acf3fe256ce266d457cad28dd6cd
Marine <5641@holbertonstudents.com>
2022-11-02 15:15:55 +0100

    4 task 18 000 th attempt de mierdaaaaaaaa

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * dog_t - creates a new dog
@@ -14,6 +15,11 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
+	char *_name = malloc(sizeof(name) + 1);
+	char *_owner = malloc(sizeof(owner) + 1);
+
+	strcpy(_name, name);
+	strcpy(_owner, owner);
 
 	if (name == 0 || owner == 0)
 		return (0);
