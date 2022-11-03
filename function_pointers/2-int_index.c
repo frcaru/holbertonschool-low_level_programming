f59e8dd7135f23fae92dbf57e6564a29e6903559
Marine <5641@holbertonstudents.com>
2022-11-03 13:49:31 +0100

    2 task correction in case of a null variable

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,10 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int j;
 
-	if (size <= 0)
+	if (array == 0 || size <= 0 || cmp == 0)
 		return (-1);
-	if (array == 0 || cmp == 0)
-		return (0);
 
 	for (i = 0; i < size; i++)
 	{
