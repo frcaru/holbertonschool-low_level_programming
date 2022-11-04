9827846917d90181205d099740fa2f31ee25d41c
Marine <5641@holbertonstudents.com>
2022-11-04 18:45:25 +0100

    3 task 2nd push

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,13 +11,13 @@
 
 int main(int argc, char **argv)
 {
+	int sum, num1, num2;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	int sum, num1, num2;
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
