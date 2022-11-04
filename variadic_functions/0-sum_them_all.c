53c96b446c6f828b060e26e1f1ddf019dfbb506a
Marine <5641@holbertonstudents.com>
2022-11-04 11:09:26 +0100

    0 task 1st push

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all its parameters
+ * @n: input
+ *
+ * Return: the sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0;
+	unsigned int i;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+	{
+	for (i = 0; i < n; i++)
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+
+	return (sum);
+}
