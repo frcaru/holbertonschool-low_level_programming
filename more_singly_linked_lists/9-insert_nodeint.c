acf78ffcf0cdd2f1299741fd126744eb3a7cacd4
Marine <5641@holbertonstudents.com>
2022-11-15 19:54:23 +0100

    correction if the list is empty

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	free(copy);
 
-	if (idx > i || *head == NULL)
+	if (idx > i)
 		return (NULL);
 
 	else if (idx == 0)
