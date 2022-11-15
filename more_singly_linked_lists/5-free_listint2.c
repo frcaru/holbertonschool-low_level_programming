40948f3753d6ffc15ee1d7d5593f34fa29d1a25c
Marine <5641@holbertonstudents.com>
2022-11-15 17:10:53 +0100

    task 5: 1st push

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: the list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
