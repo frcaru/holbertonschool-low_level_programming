7a17681fd71c830cabf34bf45ce027c372b4a749
Marine <5641@holbertonstudents.com>
2022-11-15 18:08:31 +0100

    correction of the return

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -24,7 +24,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (index > j)
 		return (NULL);
 
+	else
+	{
 	for (i = 0; i < index; i++)
 		head = head->next;
 	return (head);
+	}
 }
