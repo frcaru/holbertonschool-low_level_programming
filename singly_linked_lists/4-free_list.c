9cb417fa206717d1f17dffed80aa8ce0cb9a1e47
Marine <5641@holbertonstudents.com>
2022-11-15 16:31:19 +0100

    task 4: 1st push

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: the list to free
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current)
+	{
+		next = current->next;
+		free (current);
+		current = next;
+	}
+	head = NULL;
+}
