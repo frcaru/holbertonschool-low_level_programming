43ce28d3510b6a3acdd540418f8dd02df23d2022
Marine <5641@holbertonstudents.com>
2022-11-16 09:37:31 +0100

    task 4: free correction

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,14 +10,10 @@
 
 void free_list(list_t *head)
 {
-	list_t *current = head;
-	list_t *next;
-
-	while (current)
+	while (head != NULL)
 	{
-		next = current->next;
-		free (current);
-		current = next;
+		free(head->str);
+		free(head);
+		head = head->next;
 	}
-	head = NULL;
 }
