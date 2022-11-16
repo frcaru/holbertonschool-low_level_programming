fd59381ba2286db87f89f228e431e28b7484a449
Marine <5641@holbertonstudents.com>
2022-11-16 11:16:09 +0100

    task 5: correction of free

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -7,21 +7,16 @@
  * free_listint2 - frees a list
  * @head: the list to free
  */
+
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *next;
+	listint_t *current;
 
-	if (*head != NULL)
+	while (*head != NULL)
 	{
-	while (current)
-	{
-		next = current->next;
+		current = *head;
+		*head = (*head)->next;
 		free(current);
-		current = next;
 	}
 	*head = NULL;
-	}
-	else
-		return;
 }
