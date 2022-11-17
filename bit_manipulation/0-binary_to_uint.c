888a21ada388007806328c68784755068f092af0
Marine <5641@holbertonstudents.com>
2022-11-17 17:17:39 +0100

    Task 0: 1st push

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert a binary to an unsigned int
+ * @b: a string of 0 and 1 chars
+ *
+ * Return: converted number or 0 if b null or != 0 or 1 char
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = strlen(b) - 1;
+	int j, res = 0;
+
+	for (j = 1; b[i] != '\0'; i--, j *= 2)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		else if (b[i] == '1')
+			res += j;
+	}
+	return (res);
+
+}
