25db1cb9622f67235af838c936368d019683a1db
Marine <5641@holbertonstudents.com>
2022-11-18 09:16:11 +0100

    Task 1: 1st push

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * set_bit - set the value of a bit at 1 at a given index
+ * @n: the decimal number to convert in binary
+ * @index: the place to change the bit
+ *
+ * Return: 1 if it worked, -1 if error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int *cp = n;
+	unsigned int count = 0;
+	unsigned long int clearbit = ~(1 << index);
+	unsigned long int mask = *n & clearbit;
+
+	while (cp)
+	{
+		*cp = *cp >> 1;
+		count++;
+	}
+
+	if (index > count)
+		return (-1);
+
+	*n = mask | (1 << index);
+	return (*n);
+
+}
