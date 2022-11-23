d87ca7f245ea5df31cd02304008c7053061286f0
Marine <5641@holbertonstudents.com>
2022-11-23 15:16:12 +0100

    task 3: correction of user's permmission

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,13 +16,13 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1 = open(argv[1], O_RDWR);
+	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 00664);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
 	while (size > 0)
 	{
 		size = read(fd1, buffer, 1024);
