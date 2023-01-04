67c275374817fa1df43380047df754c736b72fc7
Marine <5641@holbertonstudents.com>
2023-01-04 17:04:31 +0100

    tache 0, 2, 3, 4, 5 et 6

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches a value in an array of
+ *                 int using the linear search
+ * @array: array of int
+ * @size: size of the array
+ * @value: the value to search for in the array
+ *
+ * Return: the first index where value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return(-1);
+
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return(i);
+	}
+
+	return(-1);
+}
---
+++ search_algorithms/README.md
@@ -0,0 +1,1 @@
+This project is about learning howw to code and use diverse searching algorithms
