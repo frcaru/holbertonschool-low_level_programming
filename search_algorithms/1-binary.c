5d09ebc70c45208a3445a478aa7f32aab82322e7
Marine <5641@holbertonstudents.com>
2023-01-05 11:08:29 +0100

    task 1: trying to fix the print

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -48,13 +48,11 @@ int binarySearch(int *array, int value, size_t low, size_t high)
 
 		else if (array[mid] < value)
 		{
-//			print_array(array, mid + 1, high);
 			return (binarySearch(array, value, mid + 1, high));
 		}
 
 		else if (array[mid] > value)
 		{
-//			print_array(array, low, mid - 1);
 			return (binarySearch(array, value, low, mid - 1));
 		}
 
