a9ec5a5718a98f8a3650c3e06acfbbfed35003ba
Marine <5641@holbertonstudents.com>
2023-01-05 11:07:02 +0100

    task 1: 1st push

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,6 +1,25 @@
 #include "search_algos.h"
 
 /**
+ * print_array - prints a partition of an array
+ * @array: an array of sorted int
+ * @low: the lowest index of the array from where to print
+ * @high: the higher index of the array to print
+ *
+ * Return: /
+ */
+
+void print_array(int *array, int low, int high)
+{
+	printf("Searching in array: ");
+
+	for (;low < high - 1; low++)
+		printf("%d, ", array[low]);
+
+	printf("%d\n", array[low]);
+}
+
+/**
  * binarySearch - searches for a value in a sorted array
  *                 of int using the binary search
  * @array: an array of sorted int
@@ -21,16 +40,24 @@ int binarySearch(int *array, int value, size_t low, size_t high)
 
 	else
 	{
+		print_array(array, low, high);
 		mid = (low + high) / 2;
 
 		if (array[mid] == value)
 			return (mid);
 
 		else if (array[mid] < value)
+		{
+//			print_array(array, mid + 1, high);
 			return (binarySearch(array, value, mid + 1, high));
+		}
 
 		else if (array[mid] > value)
+		{
+//			print_array(array, low, mid - 1);
 			return (binarySearch(array, value, low, mid - 1));
+		}
+
 		else
 			return (-1);
 	}
@@ -48,6 +75,9 @@ int binarySearch(int *array, int value, size_t low, size_t high)
 int binary_search(int *array, size_t size, int value)
 {
 	size_t low = 0;
+
+	if (array == NULL)
+		return (-1);
 
 	return (binarySearch(array, value, low, size));
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -7,6 +7,7 @@
 
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, int low, int high);
 int binarySearch(int *array, int value, size_t low, size_t high);
 int binary_search(int *array, size_t size, int value);
 
